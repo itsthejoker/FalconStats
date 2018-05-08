2386b5a3cd10d64603bcf3376807a4d2ff5514b4
Joe Kaufeld <joe.kaufeld@gmail.com>
2018-05-08 14:50:24 -0400

    Reworked for OSX and auto senses disks

--- scripts/diskspace.js
+++ scripts/diskspace.js
@@ -3,21 +3,13 @@
 // @author   https://www.reddit.com/user/LookAtMyKeyboard
 // @rewrite  https://www.reddit.com/user/cloudrac3r
 
-const util = require("util");
+// const util = require("util");
 const exec = require("child_process").exec;
 const style = require("ansi-styles");
 
-const padding = "  ";
-const barWidth = 44;
-const disks = [
-    {
-        mountpoint: "/",
-        nicemountpoint: "/",
-    },{
-        mountpoint: "/home",
-        nicemountpoint: "~",
-    }
-];
+// const padding = "  ";
+const barWidth = 60;
+
 const colors = [
     {
         range: per => per < 80,
@@ -31,25 +23,33 @@ const colors = [
     }
 ];
 
-exec("df -h "+disks.map(d => d.mountpoint).join(" "), (error, stdout, stderr) => {
-    if (error) throw error;
+exec("mount | grep disk | awk \'{print $3}\'", (error, stdout, stderr)=> {
     let lines = stdout.split("\n").filter(l => l);
-
-    let output = [lines.shift().replace("Filesystem ", "Filesystems")];
-
-    lines.forEach((line,i) => {
-        let disk = disks[i];
-
-        output.push(line.replace(disk.mountpoint, disk.nicemountpoint));
-
-        let percent = +line.match(/(\d+)%/)[1];
-        let bar = "["+Array(barWidth).fill().map((_,n) => {
-            n = n/barWidth*100;
-            if (percent >= n) return style.color[colors.find(c => c.range(n)).color].open+"="+style.color.close;
-            else return style.color.gray.open+"="+style.color.close;
-        }).join("")+"]";
-
-        output.push(bar);
+    let disks = [];
+    lines.forEach((line, i) => {
+        disks.push({"mountpoint": line, "nicemountpoint": line})
     });
-    console.log(output.join("\n"));
+
+    exec("df -h -P -g "+disks.map(d => d.mountpoint).join(" "), (error, stdout, stderr) => {
+        if (error) throw error;
+        let lines = stdout.split("\n").filter(l => l);
+
+        let output = [lines.shift().replace("Filesystem ", "Filesystems")];
+
+        lines.forEach((line,i) => {
+            let disk = disks[i];
+
+            output.push(line.replace(disk.mountpoint, disk.nicemountpoint));
+
+            let percent = +line.match(/(\d+)%/)[1];
+            let bar = "["+Array(barWidth).fill().map((_,n) => {
+                n = n/barWidth*100;
+                if (percent >= n) return style.color[colors.find(c => c.range(n)).color].open+"="+style.color.close;
+                else return style.color.gray.open+"="+style.color.close;
+            }).join("")+"]";
+
+            output.push(bar);
+        });
+        console.log(output.join("\n"));
+    });
 });
